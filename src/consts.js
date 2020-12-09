e6921dd99b38f21c2f606cc474ccf872a9ffdf3c
Gabriel <gabriel.valensuela@gmail.com>
2020-12-09 12:45:03 -0300

    feature/add base_url of heroku backend

--- src/consts.js
+++ src/consts.js
@@ -9,4 +9,5 @@ export const ROUTES = {
   CHARACTER: "/character/:id"
 };
 
-export const BASE_URL = "http://localhost:4000"
+// export const BASE_URL = "http://localhost:4000"
+export const BASE_URL = "https://marvel-comics-lib.herokuapp.com"
